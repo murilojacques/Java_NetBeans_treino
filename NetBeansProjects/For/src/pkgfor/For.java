4d4ac3c59dcf8e7a16add0afff1d01f5547bf088
Murilo Jacques <127415548+murilojacques@users.noreply.github.com>
2023-12-25 19:17:50 -0300

    a

---
+++ NetBeansProjects/For/src/pkgfor/For.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgfor;
+
+/**
+ *
+ * @author Murilo
+ */
+public class For {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+       Tela_Contar tl = new Tela_Contar();
+       tl.setVisible(true);
+    }
+    
+}
