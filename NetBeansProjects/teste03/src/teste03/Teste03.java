29be8720fca2f2164d989e98c87f18bf9869a5fc
Murilo Jacques <127415548+murilojacques@users.noreply.github.com>
2024-02-06 21:39:12 -0300

    a

--- NetBeansProjects/teste03/src/teste03/Teste03.java
+++ NetBeansProjects/teste03/src/teste03/Teste03.java
@@ -4,6 +4,8 @@
  */
 package teste03;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Murilo
@@ -14,17 +16,22 @@ public class Teste03 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       String a = "Opa Como vai, Tudo suave meu Mano?";
-       String[] r = new String[1000];
-       r = a.split(" ", r.length);
-       try{
-      for(int i=0; i<r.length; i++){    
-          System.out.println(r[i]);
-      }
-       }catch(Exception e){
-           throw e;
-       }
-       
+        /**
+         *
+         * String a = "Opa Como vai, Tudo suave meu Mano?"; String[] r = new
+         * String[1000]; r = a.split(" ", r.length); try{ for(int i=0;
+         * i<r.length; i++){ System.out.println(r[i]); } }catch(Exception e){
+         * throw e; }
+         *
+         */
+
+        Scanner variavel = new Scanner(System.in);
+        String a = "";
+        System.out.println("Digite seu nome: ");
+        a = variavel.nextLine();
+        boolean b = (a.strip().toUpperCase().contains("MARCOS"));
+        System.out.println(b);
+
     }
-    
+
 }
