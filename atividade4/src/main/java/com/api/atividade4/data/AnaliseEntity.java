833684164dc50becfb64ae7cb89afa6c8432f13d
Murilo Jacques <127415548+murilojacques@users.noreply.github.com>
2024-07-08 23:41:30 -0300

    a

---
+++ atividade4/src/main/java/com/api/atividade4/data/AnaliseEntity.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.api.atividade4.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ *
+ * @author Murilo
+ */
+@Data
+@Entity
+@Table(name="Analises")
+public class AnaliseEntity implements Serializable{
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    
+    private int filme;
+    
+    private String analise;
+    
+    private int nota;
+    
+}
