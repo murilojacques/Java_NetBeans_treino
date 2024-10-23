05fad619a02dcf9c037d2705bda0077cd7d698af
Murilo Jacques <127415548+murilojacques@users.noreply.github.com>
2024-10-23 14:40:01 -0300

    a

--- NetBeansProjects/todoSimple/src/main/resources/templates/signup.js
+++ NetBeansProjects/todoSimple/src/main/resources/templates/signup.js
@@ -3,17 +3,30 @@ async function signup() {
     let password = document.getElementById("password").value;
 
     console.log(username, password);
-    console.log(require);
-
-    var express = require('express');
-    var cors = require('cors');
-    var app = express();
-
-    app.use(cors()); // habilitando o cors da aplicação para todas as rotas
-
-    app.listen(8080, () => {
-        console.log('CORS-enabled web server listening on port 8080');
-    });
+//    console.log(require);
+//    
+//    requirejs.config({
+//    //By default load any module IDs from js/lib
+//    baseUrl: 'js/lib',
+//    //except, if the module ID starts with "app",
+//    //load it from the js/app directory. paths
+//    //config is relative to the baseUrl, and
+//    //never includes a ".js" extension since
+//    //the paths config could be for a directory.
+//    paths: {
+//        app: '../app'
+//    }
+//});
+//    
+//    var express = require(['express']);
+//    var cors = require(['cors']);
+//    var app = express();
+//
+//    app.use(cors()); // habilitando o cors da aplicação para todas as rotas
+//
+//    app.listen(8080, () => {
+//        console.log('CORS-enabled web server listening on port 8080');
+//    });
 
 
     const header = new Headers({
