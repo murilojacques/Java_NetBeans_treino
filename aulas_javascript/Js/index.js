bb4f5d1a2847736335c6db7636f16efc644c7049
Murilo Jacques <127415548+murilojacques@users.noreply.github.com>
2025-02-03 13:36:38 -0300

    a

--- aulas_javascript/Js/index.js
+++ aulas_javascript/Js/index.js
@@ -1,1 +1,28 @@
-alert('Hello World! 2');
+var valor1, valor2, total;
+
+valor1 = 7;
+valor2 = "8";
+
+//total = valor1 + valor2;
+//total = (++valor1) + valor2;
+//valor1 += valor2;
+/**
+ * valor1-=valor2;
+ * total = valor1;
+ */
+
+
+//total = (valor1 == 5) ? valor2 : 15;
+//total = (valor1 == 4) ? valor2 : 15;
+total = (valor1 !== valor2);
+
+//total = (valor1 == valor2);
+//total = (valor1 === valor2);
+
+if(valor1 > valor2){
+    document.getElementById("texto").innerHTML = "SALVE VEI";
+}
+
+
+
+alert(total);
