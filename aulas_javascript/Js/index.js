d0e19e99e2f1d768fbdf0fdfb23dd05ce66bca38
Murilo Jacques <127415548+murilojacques@users.noreply.github.com>
2025-02-03 22:44:35 -0300

    a

--- aulas_javascript/Js/index.js
+++ aulas_javascript/Js/index.js
@@ -1,28 +1,83 @@
-var valor1, valor2, total;
+function eventoClique(){
+    alert("Acionou evento onclick");
+    document.body.style.backgroundColor = "blue"
+}
 
-valor1 = 7;
-valor2 = "8";
+function eventoDblClique(){
+    alert("Acionou evento ondblclick")
+}
 
-//total = valor1 + valor2;
-//total = (++valor1) + valor2;
-//valor1 += valor2;
-/**
- * valor1-=valor2;
- * total = valor1;
- */
+function mudaCorV(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "red";
+    //document.getElementById("teste").style.backgroundColor = "red";
+}
 
+function mudaCorA(){
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "blue";
+    //document.getElementById("teste").style.backgroundColor = "blue";
+}
 
-//total = (valor1 == 5) ? valor2 : 15;
-//total = (valor1 == 4) ? valor2 : 15;
-total = (valor1 !== valor2);
-
-//total = (valor1 == valor2);
-//total = (valor1 === valor2);
-
-if(valor1 > valor2){
-    document.getElementById("texto").innerHTML = "SALVE VEI";
+function adicionaTexto(){
+    let p = document.getElementById("texto");
+    p.append("SALVE VEI")
 }
 
 
+function inputEmFoco(){
+    document.getElementById("campoTexto").value = "";
+    //let input = document.getElementById("campoTexto");
+    //input.style.backgroundColor = "orange";
+}
 
-alert(total);
+function inputPerdeFoco(){
+    document.getElementById("campoTexto").value = "SALVE MANO";
+}
+
+function onChange(){
+    console.log("mudou");
+}
+
+
+function teclaPressionada(){
+    let input = document.getElementById("campoTexto").value;
+    console.log(input)
+}
+/**
+ * const carro = {
+    marca:"Ford",
+    model:"Ka",
+    ano:2019,
+    placa:"ABC-1234",
+    buzina: function() { alert("BIiiiiiiiiiii") },
+    completo: function() {
+        return "A marca é: " + this.marca  + ", o modelo é: " + this.model + " e o Ano é: " + this.ano;
+    }
+};
+
+
+//console.log(carro["model"]);
+console.log(carro.completo());
+ */
+
+
+
+const lista = ["arroz", "feijão", "macarrão", "leite"];
+//alert(lista.length);
+//alert(lista[lista.length - 1])
+
+/*
+lista.push("bife");
+alert(lista[lista.length - 1])
+*/
+
+//alert(Array.isArray(lista));
+
+//const lista = [];
+
+//lista[0] = "arroz";
+
+//const lista = new Array("arroz", "feijão", "macarrão", "etc...");
+
+console.log(lista);
